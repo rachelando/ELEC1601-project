a5187ea2a185f3fb9e92a804faa3abced03f2239
rachelando <62196379+rachelando@users.noreply.github.com>
2020-11-20 17:56:53 +1100

    Add files via upload

---
+++ Slave_ELEC1601_project.java
@@ -0,0 +1,205 @@
+//-----------------------------------------------------------------------------------------------------------//
+//                                                                                                           //
+//  Slave_ELEC1601_Student_2019_v3                                                                           //
+//  The Instructor version of this code is identical to this version EXCEPT it also sets PIN codes           //
+//  20191008 Peter Jones                                                                                     //
+//                                                                                                           //
+//  Bi-directional passing of serial inputs via Bluetooth                                                    //
+//  Note: the void loop() contents differ from "capitalise and return" code                                  //
+//                                                                                                           //
+//  This version was initially based on the 2011 Steve Chang code but has been substantially revised         //
+//  and heavily documented throughout.                                                                       //
+//                                                                                                           //
+//  20190927 Ross Hutton                                                                                     //
+//  Identified that opening the Arduino IDE Serial Monitor asserts a DTR signal which resets the Arduino,    //
+//  causing it to re-execute the full connection setup routine. If this reset happens on the Slave system,   //
+//  re-running the setup routine appears to drop the connection. The Master is unaware of this loss and      //
+//  makes no attempt to re-connect. Code has been added to check if the Bluetooth connection remains         //
+//  established and, if so, the setup process is bypassed.                                                   //
+//                                                                                                           //
+//-----------------------------------------------------------------------------------------------------------//
+
+#include <SoftwareSerial.h>   //Software Serial Port
+
+#define RxD 7
+#define TxD 6
+#define ConnStatus A1
+
+#define DEBUG_ENABLED  1
+
+// ##################################################################################
+// ### EDIT THE LINES BELOW TO MATCH YOUR SHIELD NUMBER AND CONNECTION PIN OPTION ###
+// ##################################################################################
+
+int shieldPairNumber = 6;
+
+// CAUTION: If ConnStatusSupported = true you MUST NOT use pin A1 otherwise "random" reboots will occur
+// CAUTION: If ConnStatusSupported = true you MUST set the PIO[1] switch to A1 (not NC)
+
+boolean ConnStatusSupported = true;   // Set to "true" when digital connection status is available on Arduino pin
+
+// #######################################################
+
+// The following two string variable are used to simplify adaptation of code to different shield pairs
+
+String slaveNameCmd = "\r\n+STNA=Slave";   // This is concatenated with shieldPairNumber later
+
+SoftwareSerial blueToothSerial(RxD,TxD);
+
+#include <Servo.h>                      // Include servo library
+
+Servo servoLeft;                        // Declare left and right servos
+Servo servoRight;
+
+void setup()
+{
+    Serial.begin(9600);
+    blueToothSerial.begin(38400);                    // Set Bluetooth module to default baud rate 38400
+
+    servoLeft.attach(13);               // Attach left signal to pin 13
+    servoRight.attach(12);              // Attach right signal to pin 12
+
+    pinMode(5, OUTPUT);   // left whisker
+    pinMode(7, OUTPUT);   // right whisker
+
+    pinMode(RxD, INPUT);
+    pinMode(TxD, OUTPUT);
+    pinMode(ConnStatus, INPUT);
+
+    //  Check whether Master and Slave are already connected by polling the ConnStatus pin (A1 on SeeedStudio v1 shield)
+    //  This prevents running the full connection setup routine if not necessary.
+
+    if(ConnStatusSupported) Serial.println("Checking Slave-Master connection status.");
+
+    if(ConnStatusSupported && digitalRead(ConnStatus)==1)
+    {
+        Serial.println("Already connected to Master - remove USB cable if reboot of Master Bluetooth required.");
+    }
+    else
+    {
+        Serial.println("Not connected to Master.");
+
+        setupBlueToothConnection();   // Set up the local (slave) Bluetooth module
+
+        delay(1000);                  // Wait one second and flush the serial buffers
+        Serial.flush();
+        blueToothSerial.flush();
+    }
+}
+
+
+void loop()
+{
+    char recvChar;
+
+        if(blueToothSerial.available())   // Check if there's any data sent from the remote Bluetooth shield
+        {
+            recvChar = blueToothSerial.read();
+            if (recvChar== 'L'){
+              move('L');
+            }
+            else if (recvChar== 'R'){
+              move('R');
+            }
+            else if (recvChar== 'F'){
+              move('F');
+            }
+            else if (recvChar== 'S'){
+              move('S');
+            }
+            if (recvChar == 'Z') {
+              Serial.println("The robot has entered automated mode.");
+              automated();
+            }
+
+            Serial.print(recvChar);
+
+        }
+
+}
+
+void setupBlueToothConnection()
+{
+    Serial.println("Setting up the local (slave) Bluetooth module.");
+
+    slaveNameCmd += shieldPairNumber;
+    slaveNameCmd += "\r\n";
+
+    blueToothSerial.print("\r\n+STWMOD=0\r\n");      // Set the Bluetooth to work in slave mode
+    blueToothSerial.print(slaveNameCmd);             // Set the Bluetooth name using slaveNameCmd
+    blueToothSerial.print("\r\n+STAUTO=0\r\n");      // Auto-connection should be forbidden here
+    blueToothSerial.print("\r\n+STOAUT=1\r\n");      // Permit paired device to connect me
+
+    //  print() sets up a transmit/outgoing buffer for the string which is then transmitted via interrupts one character at a time.
+    //  This allows the program to keep running, with the transmitting happening in the background.
+    //  Serial.flush() does not empty this buffer, instead it pauses the program until all Serial.print()ing is done.
+    //  This is useful if there is critical timing mixed in with Serial.print()s.
+    //  To clear an "incoming" serial buffer, use while(Serial.available()){Serial.read();}
+
+    blueToothSerial.flush();
+    delay(2000);                                     // This delay is required
+
+    blueToothSerial.print("\r\n+INQ=1\r\n");         // Make the slave Bluetooth inquirable
+
+    blueToothSerial.flush();
+    delay(2000);                                     // This delay is required
+
+    Serial.println("The slave bluetooth is inquirable!");
+}
+
+void automated() {
+    while(1) {
+        byte wLeft = digitalRead(5);               // Copy left result to wLeft
+        byte wRight = digitalRead(7);              // Copy right result to wRight
+
+        Serial.print(wLeft);                       // Display left whisker state
+        Serial.println(wRight);                    // Display right whisker state
+        char sendChar;
+        boolean endReached = false;
+
+        if (!wLeft) {
+          sendChar = 'R';
+          move('R');
+        }
+        else if (!wRight) {
+          sendChar = 'L';
+          move('L');
+        }
+        else {
+          sendChar = 'F';
+          move('F');
+        }
+        if (endReached) {                // if robot reaches the end, send signal to master to save data (endReached NOT IMPLEMENTED)
+          sendChar = 'E';
+        }
+
+        if(blueToothSerial.available()) {   // send automated char back to master
+          blueToothSerial.print(sendChar);
+        }
+    }
+}
+
+void move(char ch) {
+
+    if (ch== 'L'){
+      servoLeft.writeMicroseconds(1500);  // Left wheel counterclockwise
+      servoRight.writeMicroseconds(1800); // Right wheel clockwise
+      //delay(4000);
+    }
+    else if (ch== 'R'){
+      servoLeft.writeMicroseconds(1200);  // Left wheel counterclockwise
+      servoRight.writeMicroseconds(1500); // Right wheel clockwise
+      //delay(4000);
+    }
+    else if (ch== 'F'){
+      servoLeft.writeMicroseconds(1300);  // Left wheel counterclockwise
+      servoRight.writeMicroseconds(1700); // Right wheel clockwise
+      //delay(4000);
+    }
+    else if (ch== 'S'){
+      servoLeft.writeMicroseconds(1470);  // Left wheel counterclockwise
+      servoRight.writeMicroseconds(1430); // Right wheel clockwise
+    }
+    delay(50);
+
+}
